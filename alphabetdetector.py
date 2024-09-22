552984f231f1968cc30c8708506c0c4bc6fc6740
amina01hu <amina01hu@gmail.com>
2024-09-22 15:37:55 -0400

    adding more letters

--- alphabetdetector.py
+++ alphabetdetector.py
@@ -1,43 +1,214 @@
-import cv2
+'''
+FUNCTION FOR PICKING UP ALPHABET IN ASL 
+'''
 
-# higher y coordinate means landmark is closer to bottom of screen 
-def asl_alphabet(hand_landmarks):
-    # LETTER A
+def asl_a(hand_landmarks):
+    """
+    Detects ASL letter A based on hand landmarks.
+    """
     landmarks = hand_landmarks.landmark
 
-    # Check that all fingertups are below their PIP
-    fingercurled = (
-        landmarks[8].y > landmarks[6].y and # index finger
-        landmarks[12].y > landmarks[10].y and # middle finger
-        landmarks[16].y > landmarks[14].y and # ring finger
-        landmarks[20].y > landmarks[18].y
+    # Check that all fingertips are below their PIP joints
+    fingers_curled = (
+        landmarks[8].y > landmarks[6].y and  # Index finger
+        landmarks[12].y > landmarks[10].y and  # Middle finger
+        landmarks[16].y > landmarks[14].y and  # Ring finger
+        landmarks[20].y > landmarks[18].y and    # Pinky
+        landmarks[8].y > landmarks[7].y and  # Index finger
+        landmarks[12].y > landmarks[11].y and  # Middle finger
+        landmarks[16].y > landmarks[15].y and  # Ring finger
+        landmarks[20].y > landmarks[19].y     # Pinky
     )
 
-    #check that all fingers are up
-
-    fingersup = (
-        landmarks[8].y < landmarks[6].y and # index finger
-        landmarks[12].y < landmarks[10].y and # middle finger
-        landmarks[16].y < landmarks[14].y and # ring finger
-        landmarks[20].y < landmarks[18].y
+    # Check if thumb is up
+    thumb_up = (
+        landmarks[4].y <= landmarks[3].y and
+        landmarks[4].y < landmarks[8].y
+        #Thumb tip is near thumb IP  
     )
 
-    # check if thumb is tucked in
-    thumbcurled = (
-        landmarks[4].x > landmarks[3].x and
-        landmarks[3].x >= landmarks[8].x
+    return fingers_curled and thumb_up
+
+
+def asl_b(hand_landmarks):
+    """
+    Detects ASL letter B based on hand landmarks.
+    """
+    landmarks = hand_landmarks.landmark
+
+    # Check if all fingers are extended (index up)
+    fingers_up = (
+        landmarks[8].y < landmarks[6].y and  # Index finger
+        landmarks[12].y < landmarks[10].y and  # Middle finger
+        landmarks[16].y < landmarks[14].y and  # Ring finger
+        landmarks[20].y < landmarks[18].y     # Pinky
     )
 
-    # check that thumb is up
+    # Check if thumb is tucked in
+    thumb_curled = (
+        landmarks[4].x > landmarks[3].x and  # Thumb base is right of IP joint
+        landmarks[3].x >= landmarks[8].x      # Thumb is to the left of the index finger
+    )
 
-    thumbup = (
-        landmarks[4].x <= landmarks[4].x  # check if thumb tip is near thumb IP
+    return fingers_up and thumb_curled
+
+
+def asl_c(hand_landmarks):
+    """
+    Detects ASL letter C based on hand landmarks.
+    """
+    landmarks = hand_landmarks.landmark
+
+    # Check for 'C' handshape
+    return (
+        landmarks[8].y > landmarks[7].y and  # Index finger tip is down
+        landmarks[12].y > landmarks[11].y and  # Middle finger tip is down
+        landmarks[16].y > landmarks[15].y and  # Ring finger tip is down
+        landmarks[20].y > landmarks[19].y and  # Pinky tip is down
+        landmarks[4].y < landmarks[3].y       # Thumb is curved
     )
 
 
-    if(fingercurled and thumbup):
+def asl_d(hand_landmarks):
+    """
+    Detects ASL letter D based on hand landmarks.
+    """
+    landmarks = hand_landmarks.landmark
+
+    # Check for 'D' handshape
+    return (
+        landmarks[8].y < landmarks[6].y and  # Index finger is up
+        landmarks[12].y > landmarks[10].y and  # Middle finger is curled
+        landmarks[16].y > landmarks[14].y and  # Ring finger is curled
+        landmarks[20].y > landmarks[18].y and  # Pinky is curled
+        landmarks[4].y < landmarks[3].y and   # Thumb is up
+        landmarks[4].x > landmarks[3].x        # Thumb is tucked in
+    )
+
+
+def asl_e(hand_landmarks):
+    """
+    Detects ASL letter E based on hand landmarks.
+    """
+    landmarks = hand_landmarks.landmark
+
+    # Check for 'E' handshape
+    return (
+        landmarks[8].y > landmarks[6].y and  # Index finger is curled down
+        landmarks[12].y > landmarks[10].y and  # Middle finger is curled down
+        landmarks[16].y > landmarks[14].y and  # Ring finger is curled down
+        landmarks[20].y > landmarks[18].y and  # Pinky is curled down
+        landmarks[4].x > landmarks[3].x and   # Thumb is to the right
+        landmarks[4].y >= landmarks[3].y   # Thumb is to the left of the index finger
+    )
+
+def asl_f(hand_landmarks):
+    """
+    Detects ASL letter F based on hand landmarks.
+    """
+    landmarks = hand_landmarks.landmark
+
+    # Check for 'F' handshape
+    return (
+        landmarks[12].y < landmarks[10].y and  # Middle finger is up
+        landmarks[16].y < landmarks[14].y and  # Ring finger is up
+        landmarks[20].y < landmarks[18].y and # pink is up
+        landmarks[8].y > landmarks[6].y and # index is down
+        landmarks[4].y < landmarks[3].y
+
+    )
+
+def asl_g(hand_landmarks):
+    """
+    Detects ASL letter G based on hand landmarks.
+    """
+    landmarks = hand_landmarks.landmark
+
+    # Check for 'g' handshape
+    return (
+        landmarks[12].x > landmarks[10].x and  # Middle finger is tucked in
+        landmarks[16].x > landmarks[14].x and  # Ring finger is tucked in
+        landmarks[20].x > landmarks[18].x and # pink finger is tucked in
+        landmarks[8].x < landmarks[6].x # index is pointed out
+
+    )
+
+def asl_h(hand_landmarks):
+    """
+    Detects ASL letter H based on hand landmarks.
+    """
+    landmarks = hand_landmarks.landmark
+
+    # Check for 'h' handshape
+    return (
+        landmarks[12].x < landmarks[10].x and  # Middle finger is pointed out
+        landmarks[16].x > landmarks[14].x and  # Ring finger is tucked in
+        landmarks[20].x > landmarks[18].x and # pink finger is tucked in
+        landmarks[8].x < landmarks[6].x # index is pointed out
+
+    )
+
+def asl_i(hand_landmarks):
+    """
+    Detects ASL letter I based on hand landmarks.
+    """
+    landmarks = hand_landmarks.landmark
+
+    # Check for 'i' handshape
+    return (
+        landmarks[8].y > landmarks[6].y and  # Index finger is up
+        landmarks[12].y > landmarks[10].y and  # Middle finger is curled
+        landmarks[16].y > landmarks[14].y and  # Ring finger is curled
+        landmarks[20].y < landmarks[18].y and  # Pinky is curled
+        landmarks[4].y < landmarks[3].y and   # Thumb is up
+        landmarks[4].x > landmarks[3].x        # Thumb is tucked in
+
+    )
+
+## def asl_j
+
+def asl_k(hand_landmarks):
+    """
+    Detects ASL letter K based on hand landmarks.
+    """
+    landmarks = hand_landmarks.landmark
+
+    # Check for 'i' handshape
+    return (
+        landmarks[8].y < landmarks[6].y and  # Index finger is up
+        landmarks[12].y < landmarks[10].y and  # Middle finger is up
+        landmarks[16].y > landmarks[14].y and  # Ring finger is curled
+        landmarks[20].y > landmarks[18].y and  # Pinky is curled
+        landmarks[4].y < landmarks[3].y and   # Thumb is up
+        landmarks[4].x > landmarks[3].x        # Thumb is tucked in
+
+    )
+
+
+def asl_alphabet(hand_landmarks):
+    """
+    Detects ASL letters A, B, C, D, and E based on hand landmarks.
+    """
+    if asl_a(hand_landmarks):
         return 'A'
-    if (fingersup and thumbcurled):
+    if asl_b(hand_landmarks):
         return 'B'
-   
-    return 0
+    if asl_c(hand_landmarks):
+        return 'C'
+    if asl_d(hand_landmarks):
+        return 'D'
+    if asl_e(hand_landmarks):
+        return 'E'
+    if asl_f(hand_landmarks):
+        return 'F'
+    if asl_g(hand_landmarks):
+        return 'G'
+    if asl_h(hand_landmarks):
+        return 'H'
+    if asl_i(hand_landmarks):
+        return 'I'
+    ## j needs motion detection
+    if asl_k(hand_landmarks):
+        return "K"
+
+    return 0  # Return None if no letter is detected
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from hand_landmarks import is_thumbs_up 
-
-from alphabetdetector import asl_alphabet
+'''
+APPLICATION FOR READING SIGN LANGUAGE FROM CAMERA
+'''
 import cv2
 import mediapipe as mp
 from google.protobuf.json_format import MessageToDict
+from hand_landmarks import is_thumbs_up
+
+from alphabetdetector import asl_alphabet
 
 # Initialize the hands module
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
-    static_image_mode=False, 
-    model_complexity=1, 
-    min_detection_confidence=0.75, 
-    min_tracking_confidence=0.75, 
+    static_image_mode=False,
+    model_complexity=1,
+    min_detection_confidence=0.75,
+    min_tracking_confidence=0.75,
     max_num_hands=2
 )
 
@@ -40,7 +43,6 @@ while True:
 
     # If hands are detected in the image
     if results.multi_hand_landmarks:
-        
         # Check if both hands are detected
         if len(results.multi_handedness) == 2:
             # Display 'Both Hands' on the image
@@ -69,18 +71,30 @@ while True:
         # Draw hand landmarks on the image for each detected hand
         for hand_landmarks in results.multi_hand_landmarks:
             # Draw the hand landmarks and connections
-            
+            for idx, landmark in enumerate(hand_landmarks.landmark):
+            # Get the height, width, and channels of the image
+                h, w, _ = img.shape
+                # Convert the normalized landmark coordinates to pixel values
+                cx, cy = int(landmark.x * w), int(landmark.y * h)
+
+                # Put the landmark index on the image at the coordinates
+                cv2.putText(img, str(idx), (cx, cy),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
             mp_drawing.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS,
                 mp_drawing.DrawingSpec(color=(255, 192, 203), thickness=2, circle_radius=2),
                 mp_drawing.DrawingSpec(color=(0, 0, 0), thickness=2))
 
             if is_thumbs_up(img, hand_landmarks.landmark, mp_hands):
                 cv2.putText(img, 'Thumbs Up!', (50, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2)
-            else: 
-                cv2.putText(img, 'Not Thumbs Up!', (50, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2)
+            else:
+                cv2.putText(img, 'Not Thumbs Up!', (50, 50),
+                            cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2)
 
             if asl_alphabet(hand_landmarks) != 0:
-                 cv2.putText(img, 'Letter ' + asl_alphabet(hand_landmarks), (50, 300), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2)
+                cv2.putText(img, 'Letter ' + asl_alphabet(hand_landmarks),
+                            (50, 300),
+                            cv2.FONT_HERSHEY_PLAIN,
+                            1, (0, 0, 0), 2)
 
             x_min, y_min = float('inf'), float('inf')
             x_max, y_max = float('-inf'), float('-inf')
@@ -95,7 +109,7 @@ while True:
                 x_max, y_max = max(x_max, cx), max(y_max, cy)
 
 
-        cv2.rectangle(img, (x_min - 10, y_min - 10), (x_max + 10, y_max + 10), (0, 0, 0), 2)    
+    cv2.rectangle(img, (x_min - 10, y_min - 10), (x_max + 10, y_max + 10), (0, 0, 0), 2)
              # Loop through all 21 landmarks
     # Display the image with hand landmarks
     cv2.imshow('Hand Recognition', img)
@@ -107,16 +121,3 @@ while True:
 # Release the video capture and destroy all OpenCV windows
 cap.release()
 cv2.destroyAllWindows()
-
-'''
-        for idx, landmark in enumerate(hand_landmarks.landmark):
-            # Get the height, width, and channels of the image
-            h, w, _ = img.shape
-            
-            # Convert the normalized landmark coordinates to pixel values
-            cx, cy = int(landmark.x * w), int(landmark.y * h)
-
-            # Put the landmark index on the image at the coordinates
-            cv2.putText(img, str(idx), (cx, cy),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-'''   
